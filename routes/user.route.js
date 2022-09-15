0ac7eba07c3f679cc57d65642910c3034a3f971b
officialzoka <golldenhouse@gmail.com>
2022-09-15 21:22:01 +0200

    change endpoints methods

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Controller = require("../controllers/user.controller");
 
 router.get("/profile", Controller.profile);
-router.get("/follow/:id", Controller.follow);
-router.get("/unfollow/:id", Controller.unfollow);
+router.post("/follow/:id", Controller.follow);
+router.post("/unfollow/:id", Controller.unfollow);
 
 module.exports = router;
