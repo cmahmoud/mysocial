e2a613a94b9a53000ec8c3c6fe89dde3ad28ee1c
officialzoka <golldenhouse@gmail.com>
2022-09-15 19:27:14 +0200

    backend setup

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    const realDB = process.env.MONGO_URI;
+    const testDB = process.env.MONGO_URI_TEST;
+    const isDevelopment = process.env.NODE_ENV === "development";
+    const uri = isDevelopment ? testDB : realDB;
+    try {
+        mongoose.connect(uri, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log(
+            `(MONGODB) connected to ${isDevelopment ? "test" : "real"} db`
+        );
+    } catch (error) {
+        console.log(error.message);
+    }
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const hpp = require("hpp");
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const environment = process.env.NODE_ENV;
+
+// middlewares
+if (environment === "development") {
+    app.use(morgan("dev"));
+}
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(hpp());
+
+app.listen(port, () => {
+    console.log(`(MySocial) running on port ${port}...`);
+});
